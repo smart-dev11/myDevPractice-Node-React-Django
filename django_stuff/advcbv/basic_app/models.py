bef2dc1809372fa6c3416ff2bf0c9672ab183e7b
harrysaito <growthharry87@gmail.com>
2018-02-06 22:13:05 +0900

    detail and crud view

--- django_stuff/advcbv/basic_app/models.py
+++ django_stuff/advcbv/basic_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class School(models.Model):
@@ -9,10 +10,13 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("basic_app:detail",kwargs={'pk':self.pk})
+
 class Student(models.Model):
     name = models.CharField(max_length=256)
     age = models.PositiveIntegerField()
-    school = models.ForeignKey(School, related_name='students')
+    school = models.ForeignKey(School,related_name='students')
 
     def __str__(self):
         return self.name
--- django_stuff/advcbv/basic_app/views.py
+++ django_stuff/advcbv/basic_app/views.py
@@ -1,29 +1,63 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
-#from django.views.generic import View, TemplateView
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse
+from django.views.generic import (View,TemplateView,
+                                ListView,DetailView,
+                                CreateView,DeleteView,
+                                UpdateView)
 from . import models
-
 # Create your views here.
-#def index(request):
-#    return render(request, 'index.html')
 
-#class CBView(View):
-#    def get(self, request):
-#        return HttpResponse("CLASS BASED VIEWS ARE COOL")
+# Original Function View:
+#
+# def index(request):
+#     return render(request,'index.html')
+#
+#
 
-# template View practice
-#class IndexView(TemplateView):
-#    template_name = 'index.html' #basic_app/index.html
+# Pretty simple right?
+class IndexView(TemplateView):
+    # Just set this Class Object Attribute to the template page.
+    # template_name = 'app_name/site.html'
+    template_name = 'index.html'
 
-#    def get_context_data(self, **kwargs):
-#        context = super().get_context_data(**kwargs)
-#        context['injectme'] = 'BASIC INJECTION!'
-#        return context
+    def get_context_data(self,**kwargs):
+        context  = super().get_context_data(**kwargs)
+        context['injectme'] = "Basic Injection!"
+        return context
 
 class SchoolListView(ListView):
-    model = models.School()
+    # If you don't pass in this attribute,
+    # Django will auto create a context name
+    # for you with object_list!
+    # Default would be 'school_list'
+
+    # Example of making your own:
+    #context_object_name = 'schools'
+    model = models.School
+
 
 class SchoolDetailView(DetailView):
-    model = models.School()
+    context_object_name = 'school_details'
+    model = models.School
     template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ("name","principal","location")
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ("name","principal")
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
+
+
+class CBView(View):
+    def get(self,request):
+        return HttpResponse('Class Based Views are Cool!')
