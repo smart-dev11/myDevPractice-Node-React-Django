bf90fb28f3bf642f746e06552fb2ee778f4d5d48
harrysaito <growthharry87@gmail.com>
2018-02-15 23:56:57 +0900

    map and propTypes

--- react/README.md
+++ react/README.md
@@ -1,3 +1,8 @@
 sudo npm install -g create-react-app
 create-react-app movie_app
 yarn start
+
+
+yarn add prop-types
+yarn cache clean
+yarn
--- react/movie_app/src/App.js
+++ react/movie_app/src/App.js
@@ -2,28 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const moviesTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "lord of the rings",
-  "Star wars"
+const movies =[
+  {
+    title:"Matrix",
+    poster:"https://upload.wikimedia.org/wikipedia/en/9/9a/The_Matrix_soundtrack_cover.jpg"
+  },
+  {
+    title:"Full Metal Jacket",
+    poster:"http://image.cine21.com/cine21/poster/2005/0607/M0010102_.jpg"
+  },
+  {
+    title:"lord of the rings",
+    poster:"http://ticketimage.interpark.com/Movie/still_image/V16/V1602287p_s01.gif"
+  },
+  {
+    title:"Star wars",
+    poster:"http://t1.daumcdn.net/movie/469c5c4957bee98cabe85e04e630f174dc0b7670"
+  }
 ]
 
-const movieImages = [
-  "https://upload.wikimedia.org/wikipedia/en/9/9a/The_Matrix_soundtrack_cover.jpg",
-  "http://image.cine21.com/cine21/poster/2005/0607/M0010102_.jpg",
-  "http://ticketimage.interpark.com/Movie/still_image/V16/V1602287p_s01.gif",
-  "http://t1.daumcdn.net/movie/469c5c4957bee98cabe85e04e630f174dc0b7670"
-]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={moviesTitles[0]} poster={movieImages[0]}/>
-        <Movie title={moviesTitles[1]} poster={movieImages[1]}/>
-        <Movie title={moviesTitles[2]} poster={movieImages[2]}/>
-        <Movie title={moviesTitles[3]} poster={movieImages[3]}/>
+        {movies.map((movie, index) =>{
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
       </div>
     );
   }
