a407fe628e7da01009be0c23aa2a6231d8f2c129
harrysaito <growthharry87@gmail.com>
2018-02-28 23:01:00 +0900

    start react native

--- react/README.md
+++ react/README.md
@@ -1,4 +1,12 @@
-Node install
+### react js app
+#### movie_app
+
+### react native app
+#### awesome-weather
+
+
+### Node install
+```
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install build-essential openssl libssl-dev pkg-config
@@ -7,13 +15,18 @@ tar -zxf node-v8.9.4.tar.gz
 ./configure
 make
 sudo make install
+```
 
-
+### start react
+```
 sudo npm install -g create-react-app
 create-react-app movie_app
 yarn start
+```
 
-
+### upgrade yarn
+```
 yarn add prop-types
 yarn cache clean
 yarn
+```
---
+++ react/awesome-weather/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+      isLoaded: false
+  }
+
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? null :(
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the awesome weather</Text>
+          </View>)
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  loading: {
+    flex:1,
+    backgroundColor:"#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
+  }
+});
