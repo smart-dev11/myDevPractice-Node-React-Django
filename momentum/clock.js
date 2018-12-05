2ebf79723435e34ca29f3ac4d36854427a9c24ce
harrysaito <growthharry87@gmail.com>
2018-12-05 22:58:06 +0900

    [js] js basics app momentum(Shwoing clock, Saving user name)

---
+++ momentum/clock.js
@@ -0,0 +1,22 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = document.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const seconds = date.getSeconds();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+
+    clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
+                            minutes < 10 ? `0${minutes}` : minutes}:${
+                            seconds < 10 ? `0${seconds}` : seconds }`;
+}
+
+
+function init() {
+    getTime()
+    setInterval(getTime, 1000);
+    // getTime();
+}
+
+init();
